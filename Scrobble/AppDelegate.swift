08af4d9973389c8645a887b93aca9ae1abc9a221
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2018-05-05 20:26:07 -0400

    Initial Commit

---
+++ Scrobble/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Scrobble
+//
+//  Created by Nishanth Shanmugham on 5/5/18.
+//  Copyright © 2018 Nishanth Shanmugham. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Scrobble/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Scrobble
+//
+//  Created by Nishanth Shanmugham on 5/5/18.
+//  Copyright © 2018 Nishanth Shanmugham. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
