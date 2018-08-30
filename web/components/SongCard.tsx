86ae3feaf03dda303ac67ed309d6409b2fe1beae
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2018-08-30 10:22:17 -0400

    flex

--- web/components/SongCard.tsx
+++ web/components/SongCard.tsx
@@ -6,24 +6,25 @@ export class SongCard extends React.Component<{song: Song, artworkBaseURL: strin
 
   private artworkURL(): string {
     if (!this.props.song.artworkHash) {
-      return SongCard.defaultArtworkPath
+      return ""
     }
     return this.props.artworkBaseURL + "/" + this.props.song.artworkHash
   }
 
   render() {
-    let s = this.props.song;
+    let s = this.props.song
+    let imgStyles = this.artworkURL() ? {backgroundImage: `url(${this.artworkURL()})`} : {backgroundColor: "#fff"};
 
     return <div className="SongCard">
-      <div>
-        <img src={this.artworkURL()}></img>
-      </div>
-      <div>{s.title}</div>
-      <div>
-        <span>{s.artistName}</span>
-        {s.artistName && s.albumTitle && <span>&nbsp;—&nbsp;</span>}
-        <span>{s.albumTitle}</span>
-      </div>
+      <div className="pict" style={imgStyles}></div>
     </div>
   }
 }
+
+      // <div>{s.title}</div>
+
+// <div>
+//         <span>{s.artistName}</span>
+//         {s.artistName && s.albumTitle && <span>&nbsp;—&nbsp;</span>}
+//         <span>{s.albumTitle}</span>
+//       </div>
