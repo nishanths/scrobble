f43449c24febe13f2dcbd2ba4b90f8aab144cb25
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2018-09-08 13:35:43 -0400

    sign in link in /u/

--- web/components/Header.tsx
+++ web/components/Header.tsx
@@ -4,7 +4,7 @@ export class Header extends React.Component<{username: string, signedIn: boolean
   render() {
     return <div className="header">
       <span className="username"><span className="emph">{this.props.username}</span><span className="rest">'s scrobbles</span></span>
-      {this.props.signedIn && <span className="settings"><a href="/">Settings</a></span>}
+      <span className="nav"><a href="/">{this.props.signedIn ? "Settings" : "Sign In"}</a></span>
     </div>
   }
 }
