3656e5149213129473de77ea5f2de2aa8e08e11f
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2018-10-06 10:00:28 -0400

    more itunes tweaks

--- appengine/itunes.go
+++ appengine/itunes.go
@@ -22,30 +22,65 @@ import (
 )
 
 var fillITunesFunc = delay.Func("fillITunes", func(ctx context.Context, namespace string, ident string) error {
-	ns, err := appengine.Namespace(ctx, namespace)
-	if err != nil {
-		return errors.Wrapf(err, "failed to make namespace for %q", namespace)
-	}
-	var s Song
-	songKey := datastore.NewKey(ns, KindSong, ident, 0, nil)
-	if err := datastore.Get(ns, songKey, &s); err != nil {
-		if err == datastore.ErrNoSuchEntity {
-			return nil // deleted sometime in between?
-		}
-		return errors.Wrapf(err, "failed to get song %s", songKey)
-	}
-	if s.iTunesFilled() {
-		return nil // nothing to do
-	}
+	done := false
 
-	// a barebones attempt at staggering
-	time.Sleep(time.Duration(rand.Intn(120e3)) * time.Millisecond)
-
-	err = datastore.RunInTransaction(ctx, func(ctx context.Context) error {
+	// Only fill from existing data.
+	if err := datastore.RunInTransaction(ctx, func(ctx context.Context) error {
 		ns, err := appengine.Namespace(ctx, namespace)
 		if err != nil {
 			return errors.Wrapf(err, "failed to make namespace for %q", namespace)
 		}
+
+		var s Song
+		songKey := datastore.NewKey(ns, KindSong, ident, 0, nil)
+		if err := datastore.Get(ns, songKey, &s); err != nil {
+			if err == datastore.ErrNoSuchEntity {
+				done = true
+				return nil // deleted sometime in between?
+			}
+			return errors.Wrapf(err, "failed to get song %s", songKey)
+		}
+		if s.iTunesFilled() {
+			done = true
+			return nil
+		}
+
+		var track ITunesTrack
+		trackKey := datastore.NewKey(ctx, KindITunesTrack, ident, 0, nil)
+		if err := datastore.Get(ctx, trackKey, &track); err != nil && err != datastore.ErrNoSuchEntity {
+			return errors.Wrapf(err, "failed to get track %s", trackKey)
+		} else if err == nil {
+			// update the song and we are done
+			s.PreviewURL = track.PreviewURL
+			s.TrackViewURL = track.TrackViewURL
+			if _, err := datastore.Put(ns, songKey, &s); err != nil {
+				return errors.Wrapf(err, "failed to put song %s", songKey)
+			}
+			done = true
+			return nil
+		}
+
+		// else err == datastore.ErrNoSuchEntity (will fetch from iTunes API below)
+		return nil
+	}, defaultTxOpts); err != nil {
+		log.Errorf(ctx, "%v", err.Error())
+		return err
+	}
+
+	if done {
+		return nil
+	}
+
+	// a barebones attempt at staggering
+	time.Sleep(time.Duration(rand.Intn(60e3)) * time.Millisecond)
+
+	// Try both.
+	if err := datastore.RunInTransaction(ctx, func(ctx context.Context) error {
+		ns, err := appengine.Namespace(ctx, namespace)
+		if err != nil {
+			return errors.Wrapf(err, "failed to make namespace for %q", namespace)
+		}
+
 		var s Song
 		songKey := datastore.NewKey(ns, KindSong, ident, 0, nil)
 		if err := datastore.Get(ns, songKey, &s); err != nil {
@@ -55,58 +90,48 @@ var fillITunesFunc = delay.Func("fillITunes", func(ctx context.Context, namespac
 			return errors.Wrapf(err, "failed to get song %s", songKey)
 		}
 		if s.iTunesFilled() {
-			return nil // nothing to do
+			return nil
 		}
 
-		var track ITunesTrack
+		// Fetch data from iTunes API
+		tctx, cancel := context.WithTimeout(ns, 5*time.Second)
+		defer cancel()
+		searchTerm := strings.Join([]string{s.Title, s.ArtistName}, " ") // including the album name produces poorer results
+		tracks, retry, err := iTunesSearchSong(tctx, searchTerm)
+		if err != nil {
+			log.Errorf(ctx, "failed to search iTunes for %q: %s (retry=%v)", searchTerm, err, retry)
+			if retry {
+				return err // returning a non-nil error causes the task to retry
+			}
+			return nil // don't bother
+		}
+
+		matchingIdx := -1
+		for idx := range tracks {
+			if !tracks[idx].ReleaseDate.IsZero() && tracks[idx].Ident() == ident && tracks[idx].HasRequiredURLs() {
+				matchingIdx = idx
+				break
+			}
+		}
+
+		if matchingIdx == -1 {
+			log.Infof(ctx, "no matching tracks found for %q (len=%d)", searchTerm, len(tracks))
+			return nil
+		}
+
 		trackKey := datastore.NewKey(ctx, KindITunesTrack, ident, 0, nil)
-		if err := datastore.Get(ctx, trackKey, &track); err != nil && err != datastore.ErrNoSuchEntity {
-			return errors.Wrapf(err, "failed to get track %s", trackKey)
-		} else if err == datastore.ErrNoSuchEntity {
-			// Need to fetch data from iTunes API
-			tctx, cancel := context.WithTimeout(ns, 5*time.Second)
-			defer cancel()
-			searchTerm := strings.Join([]string{s.Title, s.ArtistName}, " ") // including the album name produces poorer results
-			tracks, retry, err := iTunesSearchSong(tctx, searchTerm)
-			if err != nil {
-				log.Errorf(ctx, "failed to search iTunes for %q: %s (retry=%v)", searchTerm, err, retry)
-				if retry {
-					return err // returning a non-nil error causes the task to retry
-				}
-				return nil // don't bother
-			}
-
-			matchingIdx := -1
-			for idx := range tracks {
-				if !tracks[idx].ReleaseDate.IsZero() && tracks[idx].Ident() == ident && tracks[idx].HasRequiredURLs() {
-					matchingIdx = idx
-					break
-				}
-			}
-			if matchingIdx == -1 {
-				log.Infof(ctx, "no matching tracks found for %q (len=%d)", searchTerm, len(tracks))
-				return nil
-			}
-
-			if _, err := datastore.Put(ctx, trackKey, &tracks[matchingIdx]); err != nil {
-				return errors.Wrapf(err, "failed to put track %s", trackKey)
-			}
-
-			s.PreviewURL = tracks[matchingIdx].PreviewURL
-			s.TrackViewURL = tracks[matchingIdx].TrackViewURL
-		} else { // err == nil, we have it in datastore
-			s.PreviewURL = track.PreviewURL
-			s.TrackViewURL = track.TrackViewURL
+		if _, err := datastore.Put(ctx, trackKey, &tracks[matchingIdx]); err != nil {
+			return errors.Wrapf(err, "failed to put track %s", trackKey)
 		}
+
+		s.PreviewURL = tracks[matchingIdx].PreviewURL
+		s.TrackViewURL = tracks[matchingIdx].TrackViewURL
 
 		if _, err := datastore.Put(ns, songKey, &s); err != nil {
 			return errors.Wrapf(err, "failed to put song %s", songKey)
 		}
-
 		return nil
-	}, defaultTxOpts)
-
-	if err != nil {
+	}, defaultTxOpts); err != nil {
 		log.Errorf(ctx, "%v", err.Error())
 		return err
 	}
