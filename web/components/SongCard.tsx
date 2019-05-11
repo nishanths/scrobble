afe9fcce4fbd20569636cd11d4273c7a513dd3b8
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2019-05-11 12:29:07 -0400

    switch around track link and expand

--- web/components/SongCard.tsx
+++ web/components/SongCard.tsx
@@ -4,11 +4,11 @@ import { displayString as dateDisplayString } from "../src/time"
 
 export class SongCard extends React.Component<{song: Song, artworkBaseURL: string, now: Date}, {}> {
   private static readonly defaultArtworkPath = "/static/img/default-artwork.jpeg"
-  private scaleArea: HTMLDivElement|null = null
+  private trackLinkAreaElem: HTMLDivElement|null = null
 
   componentDidMount() {
     // SO says onclick on an element enables :hover on iOS
-    this.scaleArea!.setAttribute("onclick", "");
+    this.trackLinkAreaElem!.setAttribute("onclick", "");
   }
 
   private artworkURL(): string {
@@ -31,13 +31,13 @@ export class SongCard extends React.Component<{song: Song, artworkBaseURL: strin
   }
 
   private card() {
-    return <div className="scaleArea" ref={r => {this.scaleArea = r}}>
+    return <div className="scaleArea">
       {this.pict()}
-      {this.info()}
+      {this.meta()}
     </div>
   }
 
-  private info() {
+  private meta() {
     const s = this.props.song
     return <div className="meta" title={this.tooltip()}>
       <div className="title">
@@ -56,24 +56,19 @@ export class SongCard extends React.Component<{song: Song, artworkBaseURL: strin
       {backgroundImage: `url(${this.artworkURL()})`} :
       {backgroundColor: "#fff"}
 
-    let e = <div className="pict" style={imgStyles}>
-      {this.expandArea()}
+    return <div className="pict" style={imgStyles} onClick={() => {
+      console.log("clicked pict")
+    }}>
+      {this.props.song.trackViewURL && this.trackLinkArea()}
     </div>
-
-    return this.props.song.trackViewURL ?
-      <a href={this.props.song.trackViewURL} target="_blank">{e}</a> : e
   }
 
-  private expandArea() {
-    return <div className="expandArea" onClick={(e) => {
-      // prevent a[href] from opening.
-      // TODO: consider using click handlers for a[href], which should help
-      // avoid this hack.
-      e.stopPropagation()
-      console.log("clicked expand")
-    }}>
-      <div className="expand"></div>
-    </div>
+  private trackLinkArea() {
+    return <a href={this.props.song.trackViewURL} title={this.props.song.trackViewURL} target="_blank">
+      <div className="trackLinkArea" ref={r => {this.trackLinkAreaElem = r}}>
+        <div className="trackLink"></div>
+      </div>
+    </a>
   }
 
   render() {
