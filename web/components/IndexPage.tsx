770ebdbab0522db56ac8097a67514a28e34b462c
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2019-10-05 01:38:48 -0400

    10.14+ text on web page

--- web/components/IndexPage.tsx
+++ web/components/IndexPage.tsx
@@ -71,7 +71,7 @@ export class IndexPage extends React.Component<IndexPageProps, {account: Account
   }
 
   private download() {
-    return <p><a href={IndexPage.downloadURL}>Download</a> menu bar scrobble client for iTunes (macOS 10.13+)</p>
+    return <p><a href={IndexPage.downloadURL}>Download</a> menu bar scrobble client for iTunes (macOS 10.14+)</p>
   }
 
   render() {
