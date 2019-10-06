cd1e736d8dab8947b1b80de12eba2fc65a035fde
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2019-10-05 20:28:11 -0400

    remove debug

--- web/components/Songs.tsx
+++ web/components/Songs.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
-import { useSelector } from "react-redux"
 import { SongCard } from "./SongCard"
 import { Song } from "../shared/types"
-import { State } from "../redux/reducers/username"
 
 interface SongsProps {
   songs: Song[];
@@ -11,10 +9,6 @@ interface SongsProps {
 }
 
 export const Songs: React.StatelessComponent<SongsProps> = ({ songs, artworkBaseURL, now }) => {
-  useSelector((s: State) => {
-    console.log(s)
-  })
-
   return <>
     {songs.map(s => <SongCard key={s.ident} song={s} artworkBaseURL={artworkBaseURL} now={now} />)}
   </>
