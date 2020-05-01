cce47089e41ffbe3e545a4cc232e9c0367c6a99c
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2020-05-02 01:32:14 +0530

    disallow non GET

--- appengine/main.go
+++ appengine/main.go
@@ -136,6 +136,10 @@ func run(ctx context.Context) error {
 }
 
 func (s *server) buildCommitHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
 	w.Header().Set("Content-Type", "text/plain")
 	io.WriteString(w, BuildCommit)
 }
