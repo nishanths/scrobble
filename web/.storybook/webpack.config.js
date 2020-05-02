d928ecae9942c779ffc0073ea2c6b6b1f9a532f6
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2020-05-02 17:05:47 +0530

    storybook styles (#22)

--- web/.storybook/webpack.config.js
+++ web/.storybook/webpack.config.js
@@ -6,14 +6,23 @@ module.exports = ({ config }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     include: [srcPath],
-      use: [
-        {
-          loader: require.resolve('awesome-typescript-loader'),
-          options: {
-            configFileName: './.storybook/tsconfig.json',
-          }
-        },
-      ]
+    use: [
+      {
+        loader: require.resolve('awesome-typescript-loader'),
+        options: {
+          configFileName: './.storybook/tsconfig.json',
+        }
+      },
+    ]
+  });
+  config.module.rules.push({
+    test: /\.scss$/,
+    use: [
+      // creates style nodes from JS strings
+      { loader: "style-loader" },
+      { loader: "css-loader", options: { sourceMap: true }}, // translates CSS into CommonJS
+      { loader: "sass-loader", options: { sourceMap: true }} // compiles Sass to CSS
+    ]
   });
   config.resolve.extensions.push('.ts', '.tsx');
   return config;
