085f9bdc4635e2a92fd966ec538b6a1f1fa994cc
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2020-05-02 19:48:59 +0530

    support for >2 elements in segmented control (#23)

---
+++ web/components/SegmentedControl.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { SegmentedControl } from './SegmentedControl';
+
+const s = storiesOf("SegmentedControl", module)
+
+s.add("default", () => {
+	const values = ["All", "Loved", "By color"] as const
+	type V = typeof values[number]
+
+	const props = {
+		values,
+		initialValue: "All" as const,
+		afterChange: (v: V) => {
+			console.log(v)
+		},
+	}
+
+	return <SegmentedControl {...props}></SegmentedControl>
+})
