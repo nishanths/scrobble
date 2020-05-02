2fd44ddfff31e43313eababa35aee2a6e205cf72
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2020-05-02 23:34:24 +0530

    tweak colors def

--- web/components/colorpicker/color.ts
+++ web/components/colorpicker/color.ts
@@ -1,12 +1,15 @@
-export type Color =
-	| "red"
-	| "orange"
-	| "brown"
-	| "yellow"
-	| "green"
-	| "blue"
-	| "violet"
-	| "pink"
-	| "black"
-	| "gray"
-	| "white"
+export const Colors = [
+	"red",
+	"orange",
+	"brown",
+	"yellow",
+	"green",
+	"blue",
+	"violet",
+	"pink",
+	"black",
+	"gray",
+	"white",
+] as const;
+
+export type Color = typeof Colors[number]
