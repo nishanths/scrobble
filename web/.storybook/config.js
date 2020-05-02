bb2a6237d8f0a558f770f823c33fe09c82e7f9e5
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2020-05-02 17:04:45 +0530

    storybook styles

--- web/.storybook/config.js
+++ web/.storybook/config.js
@@ -1,9 +1,7 @@
 import { configure } from '@storybook/react'
 
+import '!style-loader!css-loader!sass-loader!./base.scss';
+
 const req = require.context('../components', true, /.stories.tsx$/)
-
-const loadStories = () => {
-  req.keys().forEach(req)
-}
-
+const loadStories = () => { req.keys().forEach(req) }
 configure(loadStories, module)
