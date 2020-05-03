1a0acdda68c03f1ab729d7caefd4fe8ed72a1324
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2020-05-03 15:25:03 +0530

    more redux

--- web/redux/reducers/u.ts
+++ web/redux/reducers/u.ts
@@ -1,10 +1,11 @@
 import { combineReducers } from "redux"
-import { allScrobblesReducer, lovedScrobblesReducer } from "./scrobbles"
+import { allScrobblesReducer, lovedScrobblesReducer, colorScrobblesReducer } from "./scrobbles"
 import { uargsReducer } from "./uargs"
 
 const reducer = combineReducers({
   allScrobbles: allScrobblesReducer,
   lovedScrobbles: lovedScrobblesReducer,
+  colorScrobbles: colorScrobblesReducer,
 
   // TODO: is there a better way to specify a no-op reducer for the read-only
   // preloadedState property uargs?
