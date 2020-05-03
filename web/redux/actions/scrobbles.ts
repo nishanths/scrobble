ca5ab502d6a47ce450b890d14f1c7b42e1812b6f
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2020-05-03 15:24:25 +0530

    redux

--- web/redux/actions/scrobbles.ts
+++ web/redux/actions/scrobbles.ts
@@ -13,20 +13,28 @@ export type LovedScrobblesAction =
   ReturnType<typeof lovedScrobblesSuccess> |
   ReturnType<typeof lovedScrobblesFail>
 
+export type ColorScrobblesAction =
+  ReturnType<typeof colorScrobblesStart> |
+  ReturnType<typeof colorScrobblesSuccess> |
+  ReturnType<typeof colorScrobblesFail>
+
 type AllScrobblesThunkDispatch = ThunkDispatch<PartialState, undefined, AllScrobblesAction>
 type AllScrobblesThunkResult<R> = ThunkAction<R, PartialState, undefined, AllScrobblesAction>
 
 type LovedScrobblesThunkDispatch = ThunkDispatch<PartialState, undefined, LovedScrobblesAction>
 type LovedScrobblesThunkResult<R> = ThunkAction<R, PartialState, undefined, LovedScrobblesAction>
 
-export const allScrobblesStart = (username: string) => {
+type ColorScrobblesThunkDispatch = ThunkDispatch<PartialState, undefined, ColorScrobblesAction>
+type ColorScrobblesThunkResult<R> = ThunkAction<R, PartialState, undefined, ColorScrobblesAction>
+
+const allScrobblesStart = (username: string) => {
   return {
     type: "ALL_SCROBBLES_START" as const,
     username,
   }
 }
 
-export const allScrobblesSuccess = (username: string, songs: Song[], total: number, priv: boolean) => {
+const allScrobblesSuccess = (username: string, songs: Song[], total: number, priv: boolean) => {
   return {
     type: "ALL_SCROBBLES_SUCCESS" as const,
     username,
@@ -36,21 +44,21 @@ export const allScrobblesSuccess = (username: string, songs: Song[], total: numb
   }
 }
 
-export const allScrobblesFail = (err: any) => {
+const allScrobblesFail = (err: any) => {
   return {
     type: "ALL_SCROBBLES_FAIL" as const,
     err,
   }
 }
 
-export const lovedScrobblesStart = (username: string) => {
+const lovedScrobblesStart = (username: string) => {
   return {
     type: "LOVED_SCROBBLES_START" as const,
     username,
   }
 }
 
-export const lovedScrobblesSuccess = (username: string, songs: Song[], total: number, priv: boolean) => {
+const lovedScrobblesSuccess = (username: string, songs: Song[], total: number, priv: boolean) => {
   return {
     type: "LOVED_SCROBBLES_SUCCESS" as const,
     username,
@@ -60,7 +68,7 @@ export const lovedScrobblesSuccess = (username: string, songs: Song[], total: nu
   }
 }
 
-export const lovedScrobblesFail = (err: any) => {
+const lovedScrobblesFail = (err: any) => {
   return {
     type: "LOVED_SCROBBLES_FAIL" as const,
     err,
@@ -119,9 +127,7 @@ const _fetchScrobbledSongs = async (username: string, limit: number, loved: bool
   }
 }
 
-// TODO
-
-export const colorScrobblesStart = (color: string, username: string) => {
+const colorScrobblesStart = (color: string, username: string) => {
   return {
     type: "COLOR_SCROBBLES_START" as const,
     color,
@@ -129,8 +135,56 @@ export const colorScrobblesStart = (color: string, username: string) => {
   }
 }
 
+const colorScrobblesSuccess = (color: string, username: string, hashes: ArtworkHash[], priv: boolean) => {
+  return {
+    type: "COLOR_SCROBBLES_SUCCESS" as const,
+    color,
+    username,
+    hashes,
+    private: priv,
+  }
+}
+
+const colorScrobblesFail = (err: any, color: string) => {
+  return {
+    type: "COLOR_SCROBBLES_FAIL" as const,
+    color,
+    err,
+  }
+}
+
+export const fetchColorScrobbles = (color: string, username: string): ColorScrobblesThunkResult<void> => {
+  return async (dispatch, store) => {
+    dispatch(colorScrobblesStart(color, username))
+
+    try {
+      const result = await _fetchColorScrobbles(color, username)
+      dispatch(colorScrobblesSuccess(color, username, result.hashes, result.private))
+    } catch (e) {
+      dispatch(colorScrobblesFail(e, color))
+    }
+  }
+}
+
 type FetchColorResult = {
   hashes: ArtworkHash[]
   private: boolean
   err: any | null
+}
+
+const _fetchColorScrobbles = async (color: string, username: string): Promise<FetchColorResult> => {
+  const url = "/api/v1/scrobbled?username=" + username + "&color=" + color;
+
+  const r = await fetch(url)
+  switch (r.status) {
+    case 200:
+      const hashes: string[] = await r.json()
+      return { hashes, private: false, err: null }
+    case 404:
+      return { hashes: [], private: true, err: null }
+    // TODO: if we had the ability to display toast notifications, we could show
+    // "please sign in again" for 401 status
+    default:
+      throw "bad status: " + r.status
+  }
 }
--- web/redux/reducers/scrobbles.ts
+++ web/redux/reducers/scrobbles.ts
@@ -1,4 +1,4 @@
-import { AllScrobblesAction, LovedScrobblesAction } from "../actions/scrobbles"
+import { AllScrobblesAction, LovedScrobblesAction, ColorScrobblesAction } from "../actions/scrobbles"
 import { ScrobblesState } from "../types/scrobbles"
 import { Song, ArtworkHash } from "../../shared/types"
 
@@ -16,7 +16,7 @@ const defaultState = <T extends Song | ArtworkHash>(): ScrobblesState<T> => {
 const defaultAllScrobblesState = defaultState<Song>()
 const defaultLovedScrobblesState = defaultState<Song>()
 
-export const allScrobblesReducer = (state: ScrobblesState<Song> = defaultAllScrobblesState, action: AllScrobblesAction): ScrobblesState<Song> => {
+export const allScrobblesReducer = (state = defaultAllScrobblesState, action: AllScrobblesAction): ScrobblesState<Song> => {
   switch (action.type) {
     case "ALL_SCROBBLES_START":
       return { ...state, fetching: true, done: false }
@@ -29,12 +29,12 @@ export const allScrobblesReducer = (state: ScrobblesState<Song> = defaultAllScro
   }
 }
 
-export const lovedScrobblesReducer = (state: ScrobblesState<Song> = defaultLovedScrobblesState, action: LovedScrobblesAction): ScrobblesState<Song> => {
+export const lovedScrobblesReducer = (state = defaultLovedScrobblesState, action: LovedScrobblesAction): ScrobblesState<Song> => {
   switch (action.type) {
     case "LOVED_SCROBBLES_START":
       return { ...state, fetching: true, done: false }
     case "LOVED_SCROBBLES_SUCCESS":
-      return { ...state, items: action.songs, total: action.total, private: action.private, fetching: false, error: false, done: true }
+      return { items: action.songs, total: action.total, private: action.private, fetching: false, error: false, done: true }
     case "LOVED_SCROBBLES_FAIL":
       return { ...state, fetching: false, error: true, done: true }
     default:
@@ -58,7 +58,42 @@ const colors = [
 
 const defaultColorScrobblesState = (() => {
   // create a map that maps each color to a default scrobbles state for that color
-  const m: { [k: string]: ScrobblesState<ArtworkHash> } = {}
+  const m: { [color: string]: ScrobblesState<ArtworkHash> } = {}
   colors.forEach(c => { m[c] = defaultState<ArtworkHash>() })
   return m
 })()
+
+type ColorScrobblesState = typeof defaultColorScrobblesState
+
+export const colorScrobblesReducer = (state = defaultColorScrobblesState, action: ColorScrobblesAction): ColorScrobblesState => {
+  const color = action.color
+
+  switch (action.type) {
+    case "COLOR_SCROBBLES_START": {
+      const s = copy(state)
+      s[color] = { ...s[color], fetching: true, done: false }
+      return s
+    }
+    case "COLOR_SCROBBLES_SUCCESS": {
+      const s = copy(state)
+      s[color] = { items: action.hashes, private: action.private, fetching: false, error: false, done: true }
+      return s
+    }
+    case "COLOR_SCROBBLES_FAIL": {
+      const s = copy(state)
+      s[color] = { ...s[color], fetching: false, error: true, done: true }
+      return s
+    }
+    default: {
+      return state
+    }
+  }
+}
+
+const copy = (m: ColorScrobblesState): ColorScrobblesState => {
+  const n: ColorScrobblesState = {}
+  for (const key in m) {
+    n[key] = { ...m[key] }
+  }
+  return n
+}
