f1e2404a56b0c903ed40f37197f3d7ce3a673a2b
Nishanth Shanmugham <nishanth.gerrard@gmail.com>
2020-05-03 14:28:09 +0530

    brown color leaflings ep

--- appengine/basiccolor/basiccolor_test.go
+++ appengine/basiccolor/basiccolor_test.go
@@ -137,6 +137,7 @@ func TestClosest_handpicked(t *testing.T) {
 		{7, .72, .58, Red, "Costello Music red dress"},
 		{344, .69, .26, Pink, "Everything Not Saved Will Be Lost Pt. 1"},
 		{357, .70, .32, Red, "Animal Collective Sung Tongs"},
+		{3, .73, .37, Brown, "Leaflings EP"},
 	}
 
 	undesirables := []undesirableResultTestcase{
