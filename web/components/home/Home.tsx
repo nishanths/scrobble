73d9fefa0bcf5e7effac90147cf5a8f6c6045325
Nishanth Shanmugham <nishanths@utexas.edu>
2020-05-20 21:57:05 +0530

    tagline separator

--- web/components/home/Home.tsx
+++ web/components/home/Home.tsx
@@ -13,8 +13,15 @@ const artworks = [
   { img: "onclejazz", caption: "Artwork: Oncle Jazz – Men I Trust" },
 ]
 
+const taglines = [
+  "Apple Music scrobbling service that showcases album art.",
+  "showcase your Apple Music library like never before.",
+  "a beautiful way to share your Apple Music.",
+]
+
 export const Home: React.FC<HomeProps> = ({ loginURL }) => {
   const artwork = artworks[randInt(0, artworks.length)]
+  const tagline = taglines[randInt(0, taglines.length)]
 
   return <div className="Home">
     <div className="start">
@@ -22,15 +29,15 @@ export const Home: React.FC<HomeProps> = ({ loginURL }) => {
         <img src={`/static/img/home/${artwork.img}.jpeg`} />
         <div className="caption">{artwork.caption}</div>
       </div>
-      <div className="line"><b>scrobble</b>, an Apple Music scrobbling service that showcases album art.</div>
+      <div className="line"><b>scrobble</b> — {tagline}</div>
     </div>
     <div className="sign-in">
       <GoogleSignIn loginURL={loginURL} />
     </div>
     <div className="footer">
         <div>
-          <a href={"https://scrobble.littleroot.org/u/nishanth"}><div className="item">Example profile</div></a>
           <a href={guideLink}><div className="item">How do I use Scrobble?</div></a>
+          <a href={"https://scrobble.littleroot.org/u/nishanth"}><div className="item">Example profile</div></a>
           <a href={apiDocLink}><div className="item">API doc</div></a>
           <a href={macOSAppLink}><div className="item">macOS app</div></a>
         </div>
