67e304ef38571007996282909f67ca7f6de33d35
Nishanth Shanmugham <nishanths@utexas.edu>
2020-06-20 01:57:53 +0530

    frontend

--- web/components/u/U.tsx
+++ web/components/u/U.tsx
@@ -3,8 +3,8 @@ import { RouteComponentProps } from "react-router-dom";
 import { UArgs, NProgress } from "../../shared/types"
 import { Mode, DetailKind, modeFromControlValue, fullPath } from "./shared"
 import { Scrobbles } from "./Scrobbles"
+import { Graphs } from "./Graphs"
 import { Detail } from "./Detail"
-import { Header, SegmentedControl, Top } from "./top"
 import { hexDecode } from "../../shared/util"
 import { Color } from "../colorpicker"
 import "../../scss/u/u.scss"
@@ -49,19 +49,11 @@ export const U: React.FC<UProps> = ({
 			history={history}
 		/>
 	} else if (mode === Mode.Graphs) {
-		// TODO move to separate component
-		// TODO for SegmentedControl at small widths, use flex-flow: column and different styles with a breakpoint width at ~450px
-		const header = Header(profileUsername, !!logoutURL, true)
-		const segmentedControl = SegmentedControl(mode, (v) => {
-			const m = modeFromControlValue(v)
-			history.push(fullPath(profileUsername, m, undefined, undefined)) // TODO: use last.color
-		})
-		const top = Top(header, segmentedControl, null, Mode.Graphs)
-
-		return <>
-			{top}
-			<div className="info">Graphs coming soon!</div>
-		</>
+		return <Graphs
+			profileUsername={profileUsername}
+			signedIn={!!logoutURL}
+			history={history}
+		/>
 	} else {
 		return <Scrobbles
 			profileUsername={profileUsername}
